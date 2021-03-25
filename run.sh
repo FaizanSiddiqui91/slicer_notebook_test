80d1555632dd6756ab525bd7b650b614615dee9b
FaizanSiddiqui91 <61018794+FaizanSiddiqui91@users.noreply.github.com>
2021-03-25 14:10:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ script_dir=$(cd $(dirname $0) || exit 1; pwd)
 # Set up headless environment
 source $script_dir/start-xorg.sh
 
-#/usr/bin/x11vnc -forever -rfbport $VNCPORT -display :10 -shared -bg -auth none -nopw
+/usr/bin/x11vnc -forever -rfbport $VNCPORT -display :10 -shared -bg -auth none -nopw
 #sleep 1
 
 ################################################################################
